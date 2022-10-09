c2b3c7354a9cd54726895cd1a87c19bcf0b52b65
Teun0n <simon.a.andreasen@gmail.com>
2022-10-09 15:12:51 +0200

    test passed, comments are written

--- src/max.c
+++ src/max.c
@@ -3,13 +3,13 @@
 
 int max(int* numbers, int size)
 {
-    int i;
-    int maximum=0;
-
-    for(i=0;i<size;i++){
-        if(numbers[i]>=maximum)
-        maximum=numbers[i];
+    assert(size>0); //precondition: size of array must be larger than 0.
+    int i;//counter variable counting up to position of last number in array.
+    int maximum=numbers[0];//variable holds maximum number of array, starting with first value of array.
+    for(i=0;i<size;i++) //loop runs until last number of array.
+    {
+        if(numbers[i]>=maximum)//checks if number of array is larger than maximum.
+        maximum=numbers[i];//if number if "i" position of the array is larger than maximum, maximum is redefined
     }
-
-    return maximum;
+    return maximum; //postcondition: maximum integer number of array is returned.
 }
